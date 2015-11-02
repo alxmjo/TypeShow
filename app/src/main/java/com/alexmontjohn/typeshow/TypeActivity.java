2577ae319254b87e6b90d5779021bdd24b87d74d
Alex Johnson <alexmontjohn@gmail.com>
2015-11-02 19:13:21 +0300

    Delete unused imports and declarations

--- app/src/main/java/com/alexmontjohn/typeshow/TypeActivity.java
+++ app/src/main/java/com/alexmontjohn/typeshow/TypeActivity.java
@@ -3,9 +3,6 @@ package com.alexmontjohn.typeshow;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +10,6 @@ import android.widget.EditText;
 public class TypeActivity extends Activity {
 
     private String typedString;
-    public String isNull;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
